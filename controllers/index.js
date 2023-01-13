a0a6f788b57d56401ca2007bc0e99ed8e161d32d
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-13 14:55:29 -0800

    Adds blogPosts to homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api')
 const loginRoutes = require('./login');
 const dashboardRoutes = require('./dashboard')
+const { BlogPost, User } = require('../models')
 
 router.get('/', async (req, res) => {
-  const user = {
-    username: req.session.username
-  }
-  res.render('home', { user })
+
+  const blogPostData = await BlogPost.findAll({
+    attributes: { exclude: ['password'] },
+    include: [{name: User}],
+    
+  })
+  const blogPost = blogPostData.map((blot) => blot.get({ plain: true }));
+  console.log(blogPost)
+  res.render('home', { blogPost })
 })
 
 router.use('/api', apiRoutes);
