47b1e47e3e1aeed6ff027273c7151607aed08844
Jamey Wicklund <theBluWiz@icloud.com>
2023-01-16 19:19:11 -0800

    Adds basic styling

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
+const { BlogPost, User } = require('../models')
 const withAuth = require('../Utils/helpers')
 
 router.get('/', withAuth, async (req, res) => {
   const data = {
+    userID: req.session.userID,
     username: req.session.username
   }
-  console.log(data)
+  const blogPostData = await BlogPost.findAll({
+    where: {
+      userID: data.userID,
+      // username: req.session.username
+    },
+    include: {
+      model: User,
+      attributes: ["username"],
+    },
+  });
+  const blogPost = blogPostData.map((blot) => blot.get({ plain: true }));
+  data.blogPost = blogPost
   res.render('dashboard', { data })
 });
 
